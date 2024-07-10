c0880c96c35f0c8f5a2f18a5cafe7ce2e6700fe0
Marcos Santos Moreira <marcossantosmoreira40@gmail.com>
2024-07-09 22:12:52 -0300

    Criação de template para as actions de registro de usuários

---
+++ facial-detection/resources/views/components/actions-user-registry.blade.php
@@ -0,0 +1,9 @@
+<div class="col col-12">
+    <div class="row">
+        <div class="btn-group" role="group" aria-label="Grid Actions List Users System">
+            <a class="btn btn-outline-danger"  href="{{ "#" }}" role="button">Excluir</a>
+            <a class="btn btn-outline-success" href="{{ route('user-registry.create') }}" role="button">Adicionar</a>
+            <a class="btn btn-outline-primary" href="{{ route('user-registry.index') }}" role="button">Listar</a>
+        </div>
+    </div>
+</div>
