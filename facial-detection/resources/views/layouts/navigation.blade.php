f9bc98cb5614e46e574554c883330e15aaf1a9be
Marcos Santos Moreira <marcossantosmoreira40@gmail.com>
2024-07-09 22:14:01 -0300

    Mudança de nome da tag para listagem dos usuários

--- facial-detection/resources/views/layouts/navigation.blade.php
+++ facial-detection/resources/views/layouts/navigation.blade.php
@@ -18,7 +18,7 @@
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('user-registry.index')" :active="request()->routeIs('user-registry.index')">
-                        {{ __('Users') }}
+                        {{ __('Users List') }}
                     </x-nav-link>
                 </div>
             </div>
