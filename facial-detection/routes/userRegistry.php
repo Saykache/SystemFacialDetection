5274a2afbbf5bcab28bcac21aad1b2a6fa41fd29
Marcos Santos Moreira <marcossantosmoreira40@gmail.com>
2024-07-14 08:56:24 -0300

    Rotas para o crud e para as respectivas views

--- facial-detection/routes/userRegistry.php
+++ facial-detection/routes/userRegistry.php
@@ -5,15 +5,18 @@ use App\Http\Controllers\UserRegistryController;
 Route::prefix('user-registry')->middleware('auth')->group(function () {
 
     // Rota para a página inicial do dashboard
-    Route::get('/', [UserRegistryController::class, 'index'])->name('user-registry.index');
+    Route::get('/', [UserRegistryController::class, 'index'])->name('user-registry.index'); // Página inicial listagem
 
-    // // Rotas de cadastro e update
-    Route::get('/create', [UserRegistryController::class, 'create'])->name('user-registry.create');
-    // Route::post('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    // Rotas de cadastro e update
+    Route::get('create', [UserRegistryController::class, 'create'])->name('user-registry.create'); // View de cadastro
+    Route::post('store', [UserRegistryController::class, 'store'])->name('user-registry.store'); // Cadastrar
 
-    // // Rotas de configurações
-    // Route::get('/settings', [SettingsController::class, 'index'])->name('settings.index');
-    // Route::post('/settings', [SettingsController::class, 'update'])->name('settings.update');
+    Route::get('{id}/edit', [UserRegistryController::class, 'edit'])->name('user-registry.edit'); // View de edição
+    Route::put('update', [UserRegistryController::class, 'update'])->name('user-registry.update'); // View de edição
+
+    // Rotas de exclusão
+    Route::get('{id}/delete', [UserRegistryController::class, 'delete'])->name('user-registry.delete'); // View de exclusão
+    Route::delete('destroy', [UserRegistryController::class, 'destroy'])->name('user-registry.destroy'); // Deletar
     
     // Adicione mais rotas conforme necessário
 });
